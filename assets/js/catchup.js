2da2a1d6a39653640fd91a8511ec2c560bb15550
Brandon Walsh <bmw9t@virginia.edu>
2018-05-29 12:38:47 -0400

    Fix FA icons

--- assets/js/catchup.js
+++ assets/js/catchup.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.gist').hide().before($('<a class="answer"><i class="fab fa-terminal"></i> Catch Up</a>'));
+  $('.gist').hide().before($('<a class="answer"><i class="fas fa-terminal"></i> Catch Up</a>'));
 
   $('.answer').click(function() {
     $(this).next('.gist').slideToggle('slow');
--- assets/js/hint.js
+++ assets/js/hint.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.highlight').hide().before($('<a class="answer"><i class="fab fa-terminal"></i></a>'));
+  $('.highlight').hide().before($('<a class="answer"><i class="fas fa-terminal"></i></a>'));
 
   $('.answer').click(function() {
     $(this).next('.highlight').slideToggle('slow');
